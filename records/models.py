fad5c3cd2be2044e29444d33b0e6626f7166e8c4
Mohammad Rajib <rajibkhan254051@gmail.com>
2020-09-30 20:17:51 +0600

    added record model

--- records/models.py
+++ records/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Record(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
